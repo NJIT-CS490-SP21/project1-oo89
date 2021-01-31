89c7f15e6c349a92df5c33f4bcf4693d33b0e0ed
Oscar <oo89@njit.edu>
2021-01-31 19:47:50 +0000

    Added python dotenv

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 import flask
 import os
+import random
+import spotify_api
+import genius_api
+from os import getenv
 
 app = flask.Flask(__name__)
 
+MY_ARTIST_IDS = [
+    '0vR2qb8m9WHeZ5ByCbimq2', # Reik 
+    '5Pwc4xIPtQLFEnJriah9YJ', # OneRepublic
+    '4VMYDCV2IEDYJArk749S6m', # Daddy Yankee 
+    '4wLXwxDeWQ8mtUIRPxGiD6', # Marc Anthony 
+    '6eUKZXaKkcviH0Ku9w2n3V', # Ed Sheeran 
+    '1XXUv8GRyRqOXVuDwB5QaS'  # Leoni Torres
+    ]
+
+
 @app.route('/')
+
 def index():
+    
+    # This part is to have a base from the last homework I will remove it after I finish with the other. 
     tv_lst = ["La que se Avecina", "House of Cards", "Grey's Anatomy", "The Crown", "Aida"]
     pic_url = ["/static/LaQueSeAvecina1.jpeg","/static/House.jpeg","/static/Greys.jpeg","/static/TheCrown.jpeg","/static/Aida.jpeg"]
+    
+    # Spotify info 
+    #access_token = get_access_token()
+    #artist_id = choice(ARTIST_IDS)
+    #top_track_data = get_artist_top_tracks(access_token, artist_id)
+    
     return flask.render_template(
         "index.html",
         tv_lst = tv_lst,
@@ -15,8 +38,10 @@ def index():
         pic_leng = len(pic_url)
         )
 
+
+
 app.run(
      port=int(os.getenv('PORT', 8080)),
     host=os.getenv('IP', '0.0.0.0'),
     debug=True
-    )
+    ) 
---
+++ spotify_api.py
@@ -0,0 +1,9 @@
+#from requests import get, post 
+from os import environ 
+import random
+
+# Client ID 86f5648cd4fd4be5871034a184936a10
+# Client Secret 54722e4ae7d14ef582e3c23157bf2aec
+# GET https://accounts.spotify.com/authorize
+
+#spotify_cl_id = environ['SPT_CL_ID']
