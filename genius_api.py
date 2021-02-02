ac29d5b9073a2c8e42226bc0ad87510c9a03d2b3
Oscar <oo89@njit.edu>
2021-02-02 23:29:57 +0000

    When I create the file called secret.env and put there the var I needed the program didn't work I was changing a lot of code there but I finally create a new file called .env without any name in the front and then it worked locally, let's check on heroku

--- genius_api.py
+++ genius_api.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from requests import get
 from dotenv import load_dotenv, find_dotenv
 
--- spotify_api.py
+++ spotify_api.py
@@ -1,11 +1,12 @@
 #from requests import get, post 
 import os
+from dotenv import load_dotenv, find_dotenv
 from random import choice 
 from requests import get, post 
 from decouple import config
 import json
-from dotenv import load_dotenv, find_dotenv
-#from key import CLIENT_ID, CLIENT_SECRET 
+from os import getenv
+
 
 load_dotenv(find_dotenv())
 
@@ -13,8 +14,9 @@ SPT_AUTH_URL = 'https://accounts.spotify.com/api/token'
 
 def getAccessTokenSpt():
     
-    sptClientId =  os.getenv('CLIENT_ID')
-    sptClientSecret = os.getenv('CLIENT_SECRET')
+    sptClientId=os.getenv('CLIENT_ID')
+    
+    sptClientSecret=os.getenv('CLIENT_SECRET')
     
     authResp = post(SPT_AUTH_URL, {'grant_type': 'client_credentials', 'client_id': sptClientId, 'client_secret': sptClientSecret,})
     
